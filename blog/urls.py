92f9f2dc0494ee1b62461b4550ea3e3dcd445df8
danydurand <danydurand@gmail.com>
2022-08-30 13:57:53 -0400

    Pagination, ListView for Posts
    - Paginator was implemented in Posts List
    - The original function for Post List was replaced by a class based view

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name='blog'
 
 urlpatterns = [
-    path('', views.post_list, name='post-list'),
+    path('', views.PostListView.as_view(), name='post-list'),
+    # path('', views.post_list, name='post-list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', 
         views.post_detail, name='post-detail'),
 
